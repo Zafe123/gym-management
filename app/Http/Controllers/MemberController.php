65bfcef3bcdc6aedf1ed7809303c22f776fe029b
Zafe123 <scottpilgrim712@gmail.com>
2023-01-27 10:40:16 +0800

    first commit

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    public function index()
+    {
+        $members = Member::all();
+        return view('members.index', ['members' => $members]);
+    }
+
+    public function create()
+    {
+        return view('members.create');
+    }
+
+    public function store(Request $request)
+    {
+        $member = new Member;
+        $member->id = $request->input('id');
+        $member->name = $request->input('name');
+        $member->email = $request->input('email');
+        $member->membership_type = $request->input('membership_type');
+        $member->membership_expiration = $request->input('membership_expiration');
+        $member->save();
+        return redirect()->route('members.index')->with('success', 'Member created successfully.');
+    }
+
+    public function edit($id)
+    {
+        $member = Member::find($id);
+        return view('members.edit', ['member' => $member]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $member = Member::find($id);
+        $member->id = $request->input('id');
+        $member->name = $request->input('name');
+        $member->email = $request->input('email');
+        $member->membership_type = $request->input('membership_type');
+        $member->membership_expiration = $request->input('membership_expiration');
+        $member->save();
+        return redirect()->route('members.index')->with('success', 'Member updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $member = Member::find($id);
+        $member->delete();
+        return redirect()->route('members.index')->with('success', 'Member deleted successfully.');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemberController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/members', [MemberController::class, 'index'])->name('members.index');
+Route::get('/members/create', [MemberController::class, 'create'])->name('members.create');
+Route::post('/members', [MemberController::class, 'store'])->name('members.store');
+Route::get('/members/{member}/edit', [MemberController::class, 'edit'])->name('members.edit');
+Route::put('/members/{member}', [MemberController::class, 'update'])->name('members.update');
+Route::delete('/members/{member}', [MemberController::class, 'destroy'])->name('members.destroy');
